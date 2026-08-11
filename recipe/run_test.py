43aadcae2f84841eabdeb0739de6d9b6cc7cb6ee
Jeongseok Lee <jeongseok@meta.com>
2026-08-11 15:44:13 -0700

    Enable PPC64LE support

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -22,3 +22,24 @@ sqr_d, _, _ = gpytoolbox.squared_distance(
 )
 
 np.testing.assert_allclose(np.asarray(sqr_d).reshape(-1), [0.49, 0.04])
+
+mesh_vertices = np.array(
+    [
+        [0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+    ]
+)
+mesh_faces = np.array([[0, 1, 2], [0, 2, 3]], dtype=np.int32)
+ray_origins = np.array([[0.25, 0.25, 1.0]])
+ray_directions = np.array([[0.0, 0.0, -1.0]])
+times, face_ids, barycentric = gpytoolbox.ray_mesh_intersect(
+    ray_origins,
+    ray_directions,
+    mesh_vertices,
+    mesh_faces,
+)
+np.testing.assert_allclose(times, [1.0])
+assert face_ids[0] in (0, 1)
+np.testing.assert_allclose(np.sum(barycentric, axis=1), [1.0])
